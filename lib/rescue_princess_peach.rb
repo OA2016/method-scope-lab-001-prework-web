a2155739779548da2c4f9fbf2065c378084b1bbf
oa2016 <omar.amezcua.simontech.2016@gmail.com>
2015-11-10 22:59:13 +0000

    finished method scope lab

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,10 +1,10 @@
+status = "rescued"
 def rescue_princess_peach
-  status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
 end
 
-
-
+ puts status 
+rescue_princess_peach
 
 ## Comment the below back in to see method scope in action!
 
